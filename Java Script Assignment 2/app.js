0ad6f47dd25d2790ab80a170a43a8692719dba32
Muhammad Anas <151528713+anas152@users.noreply.github.com>
2024-03-12 07:11:52 +0500

    Add files via upload

---
+++ Java Script Assignment 2/app.js
@@ -0,0 +1,118 @@
+// =======Chapter no 04 ========
+// -------Question no 01 --
+
+// var firstName, middleName, lastName;
+// var firstName = Muhammad, middleName = Anas, lastName = Soomro;  
+// console.log (firstName, middleName, lastName)
+ 
+// ------Question no 02 -------
+
+// // var var = "anas";
+// // var 1name = "anas";
+// // var firstName = "Anas";
+// // var middle_name = "anas"
+// var  fullName01 = "Muhammad Anas"
+
+// console.log(fullName01)
+
+// -----Question no 03 -----/
+
+// var heading ="Rules for naming J.S variables"
+// document.write ( "<h1>" + heading + "</h1>");
+// ------Question no 04----
+
+// var writeDocument = "Variable names can only contain $"; 
+// document.write (writeDocument)
+
+// ========Chapter no 05 =========
+
+// ---------Qestion no 01 ------------
+
+// var additon = 7 + 4
+// document.write (additon)
+
+// ------Question no 02----
+// var subtration = 10 - 4 
+// document.write (subtration)
+// ------Question no 03 -----
+// var multiplication = 10*4 
+// document.write (multiplication)
+// // --------Question no 04-------
+
+// var deivded = 10 / 4 
+// document.write (deivded)
+
+    
+// Q12 Marks Sheet
+
+// document.write("<br><br>Total marks: 780<br> Marks obtained: 670")
+
+// var marks = 780
+// var studentMarks = 804
+// var percentage = studentMarks/marks*100
+// document.write("<br><br>percentage:"+percentage)
+
+// // Q13 convert the currency
+
+// document.write("<br><br><h1>Currency in PKR</h1>")
+
+// 1 us dollar = 104.80 pakistani rupee
+// 1 Saudi riyal = 28 pakistani rupee
+
+// var usDollar = 10
+// var pakistaniRupee = 104.80
+// var pkr = usDollar*pakistaniRupee
+// var saudiRiyal = 25 
+// var pakistani = 28
+// var rupee = saudiRiyal*pakistani
+// var currency = pkr+rupee
+// document.write("<br><br>Total Currency in PKR: "+currency)
+
+// Q14 Age calculate
+
+// document.write("<br><br><h1>Age Calculator</h1>")
+
+// var currentYear = 2016
+// var birthYear = 1992 
+// var age = currentYear-birthYear
+// document.write("<br><br>Current Year: 2016 <br> Birth Year: 2005")
+// document.write("<br><br>Your Age is: "+age)
+
+// Q15 The Geometrizer
+
+// document.write("<br><br><h1>The Geometrizer</h1>")
+
+// var radius = 20
+// var circumfere = 2*3.142*radius
+// var area = 3.142*radius*radius
+
+// document.write("<br><br>Radius of a circle: 20")
+// document.write("<br><br>The  is: "+)
+// // document.write("<br><br>The area is: "+area)
+
+// Q16 Supply Calculator 
+
+// document.write("<br><br><h1>The Lifetime Supply Calculator</h1>")
+
+// var snack = "chocolate"
+// var currentAge = 15
+// var maximumAge = 65
+// var amountPerday = 3
+// var age = maximumAge-currentAge
+// var days = age*365
+// var ripe = days*amountPerday
+
+// Assignment#6
+
+
+// Q1
+
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b + b--
+// document.write("<br><br>a = 2 <br> b = 1")
+// document.write("<br><br>Result "+result)
+
+//Q2 Input a name from user &  greet the user.
+
+// var user = prompt("Assalam u walikum")
